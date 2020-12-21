a1213ee892c99d9034ddea09728ddf4538cac68a
Vadim Dissa <vadim.dissa@gmail.com>
2020-12-21 13:30:09 -0500

    Add shouldAddVertexAndDependencies

--- src/main/java/com/gmail/dissa/vadim/graph/GraphPackage.java
+++ src/main/java/com/gmail/dissa/vadim/graph/GraphPackage.java
@@ -2,5 +2,19 @@ package com.gmail.dissa.vadim.graph;
 
 import com.gmail.dissa.vadim.graph.model.VertexPackage;
 
+import java.util.List;
+
 public class GraphPackage extends Graph<VertexPackage> {
+
+    public void addVertexAndDependencies(VertexPackage vertex, List<VertexPackage> dependencies) {
+        if (vertex == null || dependencies == null) {
+            throw new IllegalArgumentException("Vertex and dependencies should not be nulls");
+        }
+        dependencies.forEach(dependency -> {
+            if(dependency != null && !dependency.equals(vertex)){
+                createEdge(vertex, dependency);
+            }
+        });
+        print();
+    }
 }
--- src/test/java/com/gmail/dissa/vadim/graph/GraphPackageTest.java
+++ src/test/java/com/gmail/dissa/vadim/graph/GraphPackageTest.java
@@ -3,6 +3,8 @@ package com.gmail.dissa.vadim.graph;
 import com.gmail.dissa.vadim.graph.model.VertexPackage;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GraphPackageTest {
@@ -19,5 +21,14 @@ public class GraphPackageTest {
         var vertex = new VertexPackage("package name");
         graph.addVertex(vertex);
         assertEquals(1, graph.getSize());
+    }
+
+    @Test
+    void shouldAddVertexAndDependencies(){
+        var vertexPackage1 = new VertexPackage("pkg1");
+        var vertexPackage2 = new VertexPackage("pkg2");
+        var vertexPackage3 = new VertexPackage("pkg3");
+        var graph = new GraphPackage();
+        graph.addVertexAndDependencies(vertexPackage1, Arrays.asList(vertexPackage2, vertexPackage3));
     }
 }
