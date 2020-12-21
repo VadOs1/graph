9af9617766dcb93d51eb759e0576fd278d2356f8
Vadim Dissa <vadim.dissa@gmail.com>
2020-12-21 13:52:34 -0500

    Add failing test

--- src/test/java/com/gmail/dissa/vadim/graph/GraphPackageTest.java
+++ src/test/java/com/gmail/dissa/vadim/graph/GraphPackageTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GraphPackageTest {
 
@@ -25,7 +26,7 @@ public class GraphPackageTest {
     }
 
     @Test
-    void shouldAddVertexAndDependencies(){
+    void shouldAddVertexAndDependencies() {
         var vertexPackage1 = new VertexPackage("pkg1");
         var vertexPackage2 = new VertexPackage("pkg2");
         var vertexPackage3 = new VertexPackage("pkg3");
@@ -38,5 +39,18 @@ public class GraphPackageTest {
         assertEquals(2, graph.getEdges(vertexPackage1).size());
         assertEquals(1, graph.getEdges(vertexPackage2).size());
         assertEquals(0, graph.getEdges(vertexPackage3).size());
+    }
+
+    @Test
+    void shouldNotDeletePackageIfThereIsDependencyOnItTest() {
+        var vertexPackage1 = new VertexPackage("pkg1");
+        var vertexPackage2 = new VertexPackage("pkg2");
+        var vertexPackage3 = new VertexPackage("pkg3");
+        var graph = new GraphPackage();
+        graph.addVertexAndDependencies(vertexPackage1, Collections.singletonList(vertexPackage2));
+        graph.addVertexAndDependencies(vertexPackage1, Arrays.asList(vertexPackage2, vertexPackage3));
+        graph.addVertexAndDependencies(vertexPackage2, Collections.singletonList(vertexPackage3));
+
+        assertThrows(IllegalArgumentException.class, () -> graph.removeVertex(vertexPackage2));
     }
 }
