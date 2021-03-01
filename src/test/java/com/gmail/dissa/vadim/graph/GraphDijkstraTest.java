3f55e9e7fba8fc65963894c27a6a6fe3f7f9c3a9
Vadim Dissa <vadim.dissa@gmail.com>
2021-03-01 13:11:36 -0500

    Make test more complicated

--- src/test/java/com/gmail/dissa/vadim/graph/GraphDijkstraTest.java
+++ src/test/java/com/gmail/dissa/vadim/graph/GraphDijkstraTest.java
@@ -61,14 +61,34 @@ public class GraphDijkstraTest {
         var v1 = new Vertex(UUID.randomUUID());
         var v2 = new Vertex(UUID.randomUUID());
         var v3 = new Vertex(UUID.randomUUID());
+        var v4 = new Vertex(UUID.randomUUID());
+        var v5 = new Vertex(UUID.randomUUID());
+        var v6 = new Vertex(UUID.randomUUID());
+        var v7 = new Vertex(UUID.randomUUID());
+        var v8 = new Vertex(UUID.randomUUID());
+        var v9 = new Vertex(UUID.randomUUID());
+        var v10 = new Vertex(UUID.randomUUID());
         graphDijkstra.addVertex(v1);
         graphDijkstra.addVertex(v2);
         graphDijkstra.addVertex(v3);
+        graphDijkstra.addVertex(v4);
+        graphDijkstra.addVertex(v5);
+        graphDijkstra.addVertex(v6);
+        graphDijkstra.addVertex(v7);
+        graphDijkstra.addVertex(v8);
+        graphDijkstra.addVertex(v9);
+        graphDijkstra.addVertex(v10);
         graphDijkstra.createEdge(v1, v2, 1.0);
         graphDijkstra.createEdge(v2, v3, 1.0);
+        graphDijkstra.createEdge(v3, v4, 1.0);
+        graphDijkstra.createEdge(v1, v5, 1.0);
+        graphDijkstra.createEdge(v5, v6, 1.0);
+        graphDijkstra.createEdge(v5, v7, 1.0);
+        graphDijkstra.createEdge(v7, v8, 1.0);
+        graphDijkstra.createEdge(v8, v9, 1.0);
 
         // THEN
-        var distanceCost = graphDijkstra.findShortestPathCost(v1, v3);
-        assertEquals(2.0, distanceCost, 0);
+        var distanceCost = graphDijkstra.findShortestPathCost(v1, v9);
+        assertEquals(4.0, distanceCost, 0);
     }
 }
