6aaf898c6f3f393bba16bd87938e154b98fdf5c6
Vadim Dissa <vadim.dissa@gmail.com>
2021-03-01 12:11:45 -0500

    Clean

--- src/main/java/com/gmail/dissa/vadim/graph/GraphDijkstra.java
+++ src/main/java/com/gmail/dissa/vadim/graph/GraphDijkstra.java
@@ -7,36 +7,37 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GraphDijkstra<T> {
-    private Map<T, Map<T, Double>> adjacencyVertexesWithCost;
+    private Map<T, Map<T, Double>> adjacencyVerticesWithCost;
 
     public GraphDijkstra() {
-        this.adjacencyVertexesWithCost = new ConcurrentHashMap<>();
+        this.adjacencyVerticesWithCost = new ConcurrentHashMap<>();
     }
 
     public void addVertex(T t) {
-        adjacencyVertexesWithCost.putIfAbsent(t, new ConcurrentHashMap<>());
+        adjacencyVerticesWithCost.putIfAbsent(t, new ConcurrentHashMap<>());
     }
 
     public void removeVertex(T t) {
-        adjacencyVertexesWithCost.keySet().remove(t);
+        adjacencyVerticesWithCost.keySet().remove(t);
     }
 
     public void createEdge(T from, T to, Double cost) {
-        adjacencyVertexesWithCost.get(from).put(to, cost);
+        adjacencyVerticesWithCost.get(from).put(to, cost);
     }
 
     public Map<T, Double> getEdges(T t) {
-        return adjacencyVertexesWithCost.get(t);
+        return adjacencyVerticesWithCost.get(t);
     }
 
     public int getVertexCount() {
-        return adjacencyVertexesWithCost.size();
+        return adjacencyVerticesWithCost.size();
     }
 
     public double findShortestPathCost(T t1, T t2) {
+        // TODO: MAKE FULL COPY OF ADJACENCY VERTICES
+        Map<T, Double> costs = createInitialCostsMap(adjacencyVerticesWithCost);
+        Map<T, T> parents = createInitialParentsMap(adjacencyVerticesWithCost);
         Set<T> visited = new HashSet<>();
-        Map<T, Double> costs = createInitialCostsMap();
-        Map<T, T> parents = createInitialParentsMap();
 
         T t = getLowestCostVertex(costs, visited);
 
@@ -48,15 +49,15 @@ public class GraphDijkstra<T> {
         return 0.0;
     }
 
-    private Map<T, Double> createInitialCostsMap() {
+    private Map<T, Double> createInitialCostsMap(Map<T, Map<T, Double>> adjacencyVerticesWithCost) {
         var map = new HashMap<T, Double>();
-        adjacencyVertexesWithCost.keySet().forEach(v -> map.put(v, Double.POSITIVE_INFINITY));
+        adjacencyVerticesWithCost.keySet().forEach(v -> map.put(v, Double.POSITIVE_INFINITY));
         return map;
     }
 
-    private Map<T, T> createInitialParentsMap() {
+    private Map<T, T> createInitialParentsMap(Map<T, Map<T, Double>> adjacencyVerticesWithCost) {
         var map = new HashMap<T, T>();
-        adjacencyVertexesWithCost.keySet().forEach(v -> map.put(v, null));
+        adjacencyVerticesWithCost.keySet().forEach(v -> map.put(v, null));
         return map;
     }
 
