d1b1b150a6d718f41c4c386a7fa6f59f47d306bc
Vadim Dissa <vadim.dissa@gmail.com>
2021-03-01 11:59:36 -0500

    Add lowest cost calculation

--- src/main/java/com/gmail/dissa/vadim/graph/GraphDijkstra.java
+++ src/main/java/com/gmail/dissa/vadim/graph/GraphDijkstra.java
@@ -1,9 +1,9 @@
 package com.gmail.dissa.vadim.graph;
 
-import com.gmail.dissa.vadim.graph.model.Vertex;
-
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GraphDijkstra<T> {
@@ -33,9 +33,46 @@ public class GraphDijkstra<T> {
         return adjacencyVertexesWithCost.size();
     }
 
-    public double findShortestPathCost(Vertex v1, Vertex v2) {
-        Map<T, Double> costs = new HashMap<>();
-        Map<T, T> parents = new HashMap<>();
+    public double findShortestPathCost(T t1, T t2) {
+        Set<T> visited = new HashSet<>();
+        Map<T, Double> costs = createInitialCostsMap();
+        Map<T, T> parents = createInitialParentsMap();
+
+        T t = getLowestCostVertex(costs, visited);
+
+        while(t != null){
+            visited.add(t);
+            t = null;
+        }
+
         return 0.0;
+    }
+
+    private Map<T, Double> createInitialCostsMap() {
+        var map = new HashMap<T, Double>();
+        adjacencyVertexesWithCost.keySet().forEach(v -> map.put(v, Double.POSITIVE_INFINITY));
+        return map;
+    }
+
+    private Map<T, T> createInitialParentsMap() {
+        var map = new HashMap<T, T>();
+        adjacencyVertexesWithCost.keySet().forEach(v -> map.put(v, null));
+        return map;
+    }
+
+    private T getLowestCostVertex(Map<T, Double> costsMap, Set<T> visited) {
+        var lowestCost = Double.POSITIVE_INFINITY;
+        T lowestCostVertex = null;
+
+        for (Map.Entry<T, Double> entry : costsMap.entrySet()) {
+            var key = entry.getKey();
+            var value = entry.getValue();
+            if (!visited.contains(key) && value < lowestCost) {
+                lowestCost = value;
+                lowestCostVertex = key;
+            }
+        }
+
+        return lowestCostVertex;
     }
 }
