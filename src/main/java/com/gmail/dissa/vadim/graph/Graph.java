db78f874581e932c2c1cac5e6d1512384b2ac46a
Vadim Dissa <vadim.dissa@gmail.com>
2021-03-03 18:57:25 -0500

    Clean

--- src/main/java/com/gmail/dissa/vadim/graph/Graph.java
+++ src/main/java/com/gmail/dissa/vadim/graph/Graph.java
@@ -1,5 +1,6 @@
 package com.gmail.dissa.vadim.graph;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -23,31 +24,38 @@ public class Graph<T> {
         adjacencyVertices.putIfAbsent(t, ConcurrentHashMap.newKeySet());
     }
 
-    public void createEdge(T from, T to) {
-        checkIfExist(from);
-        checkIfExist(to);
+    public synchronized void createEdge(T from, T to) {
+        addVertex(from);
+        addVertex(to);
         adjacencyVertices.get(from).add(to);
     }
 
+    public synchronized boolean removeVertexWithUsageCheck(T t) {
+        checkIfExist(t);
+        if (isReferenced(t)) {
+            return false;
+        } else {
+            Set<T> edges = adjacencyVertices.get(t);
+            adjacencyVertices.keySet().remove(t);
+            for (T edge : edges) {
+                removeVertexWithUsageCheck(edge);
+            }
+            return true;
+        }
+    }
+
     public Set<T> getEdges(T t) {
-        return adjacencyVertices.get(t);
+        return new HashSet<>(adjacencyVertices.get(t));
     }
 
     public int getVertexCount() {
         return adjacencyVertices.size();
     }
 
-    public boolean removeVertexWithUsageCheck(T t) {
-        checkIfExist(t);
-        if (isReferenced(t)) {
-            return false;
-        } else {
-            Set<T> edges = getEdges(t);
-            adjacencyVertices.keySet().remove(t);
-            for (T edge : edges) {
-                removeVertexWithUsageCheck(edge);
-            }
-            return true;
+    private void checkIfExist(T t) {
+        Set<T> edges = adjacencyVertices.get(t);
+        if (edges == null) {
+            throw new IllegalArgumentException("Doesn't exist");
         }
     }
 
@@ -58,12 +66,5 @@ public class Graph<T> {
             }
         }
         return false;
-    }
-
-    private void checkIfExist(T t){
-        Set<T> edges = adjacencyVertices.get(t);
-        if (edges == null) {
-            throw new IllegalArgumentException("Doesn't exist");
-        }
     }
 }
